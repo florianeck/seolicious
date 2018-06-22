87ffb1300795a6cd8bdc0c7fe5d74db758fbef05
Florian Eck <florian.eck@el-digital.de>
2018-06-22 16:48:05 +0200

    allow custom params

--- lib/seolicious/controller_ext.rb
+++ lib/seolicious/controller_ext.rb
@@ -4,7 +4,7 @@ module Seolicious
     extend ActiveSupport::Concern
     
     def load_seo_page(options = {include_params: false})
-      @seo_page = SeoPage.from_params(params, options)
+      @seo_page = SeoPage.from_params(options[:params] || params, options)
       @seo_page.update_column(:name, request.path) if @seo_page.name.blank?
     end
 
